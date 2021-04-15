734edff275fc7fa417ca28fdba49de91d83ff95c
David Průša <cave.bushman@gmail.com>
2021-04-15 07:28:35 +0200

    fix fee for under classes

--- event/views.py
+++ event/views.py
@@ -114,7 +114,7 @@ def EntryView(request, pk):
         sum_20 = riders_20.count()
         sum_24 = riders_24.count()
         for rider_20 in riders_20:
-            if re.search("Elite", rider_20.class_20):
+            if re.search("Elite", rider_20.class_20) or re.search("Under", rider_20.class_20):
                 sum_fee += event.fee_elite
             elif re.search("Men", rider_20.class_20) or re.search("Women", rider_20.class_20):
                 sum_fee += event.fee_men_women
