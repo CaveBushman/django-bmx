90b1d713c04fb0a45b8f144e5ba9d48ac5c5800e
Temp1ar <temp1ar@temp1ar.black-ops.eu>
2022-04-04 19:30:24 +0200

    tailwind upgrade

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -6,6 +6,13 @@
  */
 
 module.exports = {
+    /**
+     * Stylesheet generation mode.
+     *
+     * Set mode to "jit" if you want to generate your styles on-demand as you author your templates;
+     * Set mode to "aot" if you want to generate the stylesheet in advance and purge later (aka legacy mode).
+     */
+
     content: [
         /**
          * HTML. Paths to Django template files that will contain Tailwind CSS classes.
@@ -14,13 +21,13 @@ module.exports = {
         /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
         '../templates/**/*.html',
 
-        /* 
+        /*
          * Main templates directory of the project (BASE_DIR/templates).
          * Adjust the following line to match your project structure.
          */
         '../../templates/**/*.html',
-        
-        /* 
+
+        /*
          * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
          * Adjust the following line to match your project structure.
          */
@@ -42,6 +49,9 @@ module.exports = {
         // '../../**/*.py'
     ],
     theme: {
+        extend: {},
+    },
+    variants: {
         extend: {},
     },
     plugins: [
