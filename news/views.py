b033994f8de04c767a8f0227739bf4c1a0f55a7f
CaveBushman <david@black-ops.eu>
2023-10-06 22:35:55 +0200

    dark mode

--- news/views.py
+++ news/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404 
+from django.http import HttpResponseRedirect
 from django.core.paginator import Paginator, EmptyPage
 from event.models import Event
 from rider.models import Rider
@@ -9,6 +10,13 @@ from datetime import date
 
 
 # Create your views here.
+
+def change_theme(request):
+    if 'is_dark_mode' in request.session:
+        request.session['is_dark_mode'] = not request.session['is_dark_mode']
+    else:
+        request.session['is_dark_mode'] = True
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 def homepage_view(request):
     this_year = date.today().year
