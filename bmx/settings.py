fed434f14b8b3f939acc34d49ed533d040f63312
David Prusa <cave.bushman@gmail.com>
2023-10-22 09:17:36 +0200

    fix results

--- bmx/settings.py
+++ bmx/settings.py
@@ -29,12 +29,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deploymentpyt/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+#SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "1"
 
 # STRIPE KEYS
 if  DEBUG:
