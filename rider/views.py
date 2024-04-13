7373a06dac131f214db21559aa3523a9c665512b
CaveBushman <david@black-ops.eu>
2024-04-13 07:16:31 +0200

    new export Count riders by classes

--- rider/views.py
+++ rider/views.py
@@ -1,7 +1,8 @@
 import os
 
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.views.decorators.cache import cache_control
@@ -9,7 +10,8 @@ from django.contrib.admin.views.decorators import staff_member_required
 
 from bmx import settings
 from .models import Rider
-from .rider import two_years_inactive, CheckValidLicenceThread, Participation, Cruiser, RiderSetClassesThread
+from .rider import two_years_inactive, CheckValidLicenceThread, Participation, Cruiser, RiderSetClassesThread, \
+    first_line_riders_by_club_and_class
 from func.rider import set_all_riders_classes
 from club.models import Club
 import json
@@ -20,9 +22,11 @@ from datetime import date
 import requests
 import requests.packages
 from decouple import config
+from openpyxl import Workbook
 
 # Global variables
 now = date.today().year
+
 
 # Create your views here.
 
@@ -40,16 +44,16 @@ def rider_detail_view(request, pk):
     data = {'rider': rider, 'results': results}
     return render(request, 'rider/rider-detail.html', data)
 
+
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 @staff_member_required
 def rider_admin(request):
-
     return render(request, 'rider/rider-admin.html')
 
 
 def rider_new_view(request):
     """ View for new rider form """
-    
+
     # User fill data and send form
     if request.method == "POST":
 
@@ -79,7 +83,8 @@ def rider_new_view(request):
             num9 = request.POST['num9']
             num10 = request.POST['num10']
             num11 = request.POST['num11']
-            uci_id = str(num1)+str(num2)+str(num3)+str(num4)+str(num5)+str(num6)+str(num7)+str(num8)+str(num9)+str(num10)+str(num11)
+            uci_id = str(num1) + str(num2) + str(num3) + str(num4) + str(num5) + str(num6) + str(num7) + str(
+                num8) + str(num9) + str(num10) + str(num11)
             # url_uci = f"https://ucibws.uci.ch/api/contacts/riders?filter.uciid={uci_id}"
 
             username = config('LICENCE_USERNAME')
@@ -90,14 +95,13 @@ def rider_new_view(request):
             data_json = requests.get(url_uciid, auth=basicAuthCredentials, verify=False)
             data_json = data_json.text
             print(type(data_json))
-            print (data_json)
+            print(data_json)
             if "\Http_NotFound" in data_json:
-                print ("Tato licence neexistuje")
+                print("Tato licence neexistuje")
                 message = f"Licence UCI ID: {uci_id} nebyla Českým svazem cyklistiky vystavena. Zkuste to znovu se správným číslem nebo kontaktujte Komisi BMX Českého svazu cyklistiky."
                 data = {'message': message}
                 return render(request, 'rider/rider-new-error.html', data)
             data_json = json.loads(data_json)
-
 
             gender = data_json['sex']
             if gender['code'] == "F":
@@ -107,8 +111,8 @@ def rider_new_view(request):
 
             try:
                 data_new_rider = {'first_name': data_json['firstname'], 'last_name': data_json['lastname'],
-                                 'date_of_birth': data_json['birth'][0:10], 'clubs': clubs,
-                                 'free_plates': free_plates, 'uci_id': data_json['uci_id'], 'gender': gender}
+                                  'date_of_birth': data_json['birth'][0:10], 'clubs': clubs,
+                                  'free_plates': free_plates, 'uci_id': data_json['uci_id'], 'gender': gender}
                 request.session['first_name'] = data_json['firstname']
                 request.session['last_name'] = data_json['lastname'].capitalize()
                 request.session['date_of_birth'] = data_json['birth'][0:10]
@@ -123,8 +127,8 @@ def rider_new_view(request):
 
                 if rider_exist:
                     message = f"Jezdec/jezdkyně {rider.first_name} {rider.last_name}, UCI ID {rider.uci_id}, již má přiděleno permanentní startovní číslo. Kontatujte Komisi BMX Českého svazu cyklistiky."
-                    data={'message':message}
-                    return render (request, 'rider/rider-new-error.html', data)
+                    data = {'message': message}
+                    return render(request, 'rider/rider-new-error.html', data)
 
                 return render(request, 'rider/rider-new-2.html', data_new_rider)
             except Exception as error:
@@ -142,7 +146,7 @@ def rider_new_view(request):
                                   'date_of_birth': request.session['date_of_birth'],
                                   'gender': request.session['gender'],
                                   'clubs': clubs, 'free_plates': free_plates, 'uci_id': request.session['uci_id'],
-                }
+                                  }
                 return render(request, 'rider/rider-new-2.html', data_new_rider)
             else:
                 # TODO: Dodělat ověření správnosti e-mailové adresy
@@ -161,7 +165,8 @@ def rider_new_view(request):
 
             if ('is20') not in request.POST and ('is24') not in request.POST:
                 print("Není vybrána kategorie")
-                messages.error(request, "Nevyplnil/a jsi, zda budeš jezdit na 20-ti palcovém kole či na Cruiseru. Jedná se o povinný údaj.")
+                messages.error(request,
+                               "Nevyplnil/a jsi, zda budeš jezdit na 20-ti palcovém kole či na Cruiseru. Jedná se o povinný údaj.")
                 data_new_rider = {'first_name': request.session['first_name'],
                                   'last_name': request.session['last_name'],
                                   'date_of_birth': request.session['date_of_birth'],
@@ -172,7 +177,7 @@ def rider_new_view(request):
 
             if request.POST['club'] == "Vyber...":
                 print("Není vybrán klub")
-                messages.error(request,  "Nevybral jsi svůj klub. Jedná se o povinný údaj.")
+                messages.error(request, "Nevybral jsi svůj klub. Jedná se o povinný údaj.")
                 data_new_rider = {'first_name': request.session['first_name'],
                                   'last_name': request.session['last_name'],
                                   'date_of_birth': request.session['date_of_birth'],
@@ -225,7 +230,7 @@ def rider_new_view(request):
 def inactive_riders_views(request):
     """ Function for views inactive riders, only request params"""
     inactive_riders = two_years_inactive()
-    data={'riders': inactive_riders, 'sum': len(inactive_riders)}
+    data = {'riders': inactive_riders, 'sum': len(inactive_riders)}
     return render(request, 'rider/rider-inactive.html', data)
 
 
@@ -264,6 +269,7 @@ def recalculate_riders_classes(request):
     data = {}
     return render(request, 'rider/rider-success.html', data)
 
+
 @staff_member_required
 def calculate_cruiser_median(request):
     cruiser = Cruiser()
@@ -275,3 +281,68 @@ def calculate_cruiser_median(request):
     return render(request, 'rider/rider-cruiser.html', data)
 
 
+@login_required
+def riders_by_class_and_club(request):
+    if not request.user.is_admin or not request.user.is_superuser:
+        return redirect('news:homepage')
+
+    clubs = Club.objects.filter(is_active=True).order_by('team_name')
+    file_name = 'RIDERS_BY_CLUB_AND_CLASS.xlsx'
+
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+
+    wb = Workbook()
+    ws = wb.active
+
+    first_line_riders_by_club_and_class(ws)
+
+    row = 2
+    for club in clubs:
+        ws.cell(row, 1, club.team_name)
+        ws.cell(row, 2, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Boys 6").count())
+        ws.cell(row, 3, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Boys 7").count())
+        ws.cell(row, 4, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Girls 7").count())
+        ws.cell(row, 5, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Boys 8").count())
+        ws.cell(row, 6, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Girls 8").count())
+        ws.cell(row, 7, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Boys 9").count())
+        ws.cell(row, 8, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Girls 9").count())
+        ws.cell(row, 9, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Boys 10").count())
+        ws.cell(row, 10, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Girls 10").count())
+        ws.cell(row, 11, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Boys 11").count())
+        ws.cell(row, 12, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Girls 11").count())
+        ws.cell(row, 13, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Boys 12").count())
+        ws.cell(row, 14, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Girls 12").count())
+        ws.cell(row, 15, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Boys 13").count())
+        ws.cell(row, 16, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Girls 13").count())
+        ws.cell(row, 17, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Boys 14").count())
+        ws.cell(row, 18, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Girls 14").count())
+        ws.cell(row, 19, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Boys 15").count())
+        ws.cell(row, 20, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Girls 15").count())
+        ws.cell(row, 21, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Boys 16").count())
+        ws.cell(row, 22, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Girls 16").count())
+        ws.cell(row, 23, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Men 17-24").count())
+        ws.cell(row, 24,
+                Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Women 17-24").count())
+        ws.cell(row, 25, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Men 25-29").count())
+        ws.cell(row, 26,
+                Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Women 25 and over").count())
+        ws.cell(row, 27, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Men 30-34").count())
+        ws.cell(row, 28,
+                Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Men 35 and over").count())
+        ws.cell(row, 29,
+                Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Men Junior").count())
+        ws.cell(row, 30,
+                Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Women Junior").count())
+        ws.cell(row, 31,
+                Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Men Under 23").count())
+        ws.cell(row, 32,
+                Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Women Under 23").count())
+        ws.cell(row, 33, Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Men Elite").count())
+        ws.cell(row, 34,
+                Rider.objects.filter(is_active=True, is_approwe=True, club=club, class_20="Women Elite").count())
+
+        row += 1
+    wb.save(response)
+
+    return response
