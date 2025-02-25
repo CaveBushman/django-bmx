206f49b86ef9d4d87041790008268dfefb471e01
CaveBushman <david@black-ops.eu>
2025-02-25 19:12:15 +0100

    change intent instead id

--- event/views.py
+++ event/views.py
@@ -1185,15 +1185,12 @@ def success_credit_view(request):
 
     credit_transactions =  CreditTransaction.objects.filter(Q(transaction_date__year=date.today().year,
                                           transaction_date__month=date.today().month,
-                                          transaction_date__day=date.today().day,) |
+                                          transaction_date__day=date.today().day,
+                                          payment_complete=False, ) |
                                         (Q(transaction_date__year=date.today().year,
                                            transaction_date__month=date.today().month,
                                            transaction_date__day=date.today().day - 1,
-                                           payment_complete=False,))|
-                                        (Q(transaction_date__year=date.today().year,
-                                           transaction_date__month=date.today().month,
-                                           transaction_date__day=date.today().day - 2,
-                                           payment_complete=False,)))
+                                           payment_complete=False, )))
 
     for credit_transaction in credit_transactions:
         try:
