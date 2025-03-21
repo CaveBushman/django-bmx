e48b3cc4b7ad37a11021b7de2984b3a917bb94d0
David Prusa <cave.bushman@gmail.com>
2025-03-21 23:11:31 +0100

    remove compressor

--- staticfiles/js/dropdown.js
+++ staticfiles/js/dropdown.js
@@ -1,22 +1,24 @@
 const dropdowns = document.querySelectorAll('.dropdown-main')
-console.log("Tlačítko dropdown")
+
 dropdowns.forEach(dropdown => {
     const btn = dropdown.querySelector('.dropdown-btn')
     const content = dropdown.querySelector('.dropdown-content')
     
     btn.addEventListener('click', () => {
+        dropdowns.forEach(d => {
+            if(d !== dropdown) {
+                const c = d.querySelector('.dropdown-content')
+                c.classList.add('hidden')
+            }
+        })
         content.classList.toggle('hidden')
-        btn.classList.toggle('bg-white')
-        btn.classList.toggle('text-green-600')
-        console.log(btn.classList)
     })
 
     document.body.addEventListener('click', (e) => {
-        if(e.target.classList.contains('dropdown-btn')) return
+        if(e.target.classList.contains('dropdown-btn') || e.target.parentNode.classList.contains('dropdown-btn')) return
         content.classList.add('hidden')
-        btn.classList.remove('bg-white')
-        btn.classList.remove('text-green-600')
+
     })
 })
 
-console.log("Script Dropdown nahrán v delší verzi")
+console.log("Script Dropdown nahrán")
